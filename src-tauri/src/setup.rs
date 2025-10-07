2239f5058d824713f74a0406b8fbf4c129e187a3
mslxl <i@mslxl.com>
2025-10-07 15:27:54 +0800

    feat: support compile on macos

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -138,7 +138,7 @@ pub fn setup_decorum(app: &tauri::App) -> Result<()> {
 
         // Set window level
         // NSWindowLevel: https://developer.apple.com/documentation/appkit/nswindowlevel
-        main_window.set_window_level(25).unwrap()
+        main_window.set_window_level(25).unwrap();
     }
 
     Ok(())
